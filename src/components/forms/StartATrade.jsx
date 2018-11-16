87fc30b28e0318a263553e2663844824e38392ff
Alan <boyce.alan15@gmail.com>
2018-11-16 00:10:41 -0500

    logic for trade buttons

--- src/components/forms/StartATrade.jsx
+++ src/components/forms/StartATrade.jsx
@@ -9,70 +9,74 @@ import { InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap';
 export default class StartATrade extends React.Component {
   constructor(props) {
     super(props);
-
+    // this.showSkillForm = this.showSkillForm.bind(this);
     this.state = {
       trade: {
+        name: "",
         numSkills: 0,
-        numItems: "",
-        numCryptos: ""
+        numItems: 0,
+        numCryptos: 0,
       },
+      thingsInTrade: [],
       loading: false,
-      errors: {
-        username: "",
-        email: "",
-        password: "",
-        confirmPassword: ""
-      } // when we pass errors into setState, the new error object will replace the old one in the state object
+      skillButton: {
+        isClicked: false,
+
+      }
+      // errors: {
+      //
+      // } // when we pass errors into setState, the new error object will replace the old one in the state object
     };
+    // alert('skill clicked');
   }
 
-  onChange = e => {
-    this.setState({
-      data: { ...this.state.data, [e.target.name]: e.target.value }
-    });
+  submitButtonClicked = (event) => {
+      console.log('Sumbit Button');
+      // sidebar.style.top = '65px';
+      // sidebar.style.display = 'block';
+      sidebar.style.visibility = 'visible';
   }
 
-  onSubmit = (e) => {
-    //alert('submit button clicked');
-    // creating an errors object that will be asigned the return value of the validate function (of this object)
-    const errors = this.validate(this.state.data, e);
-    // // when we pass errors into setState, the new error object will replace the old one in the state object
-    this.setState({ errors });
-  }
-
-  // validate = (data, e) => {
-  //   const errors = {};
-  //   if(data.username.length<1){
-  //     errors.password = "PASSWORD Input is blank. Password length: " + data.password.length;
-  //     alert(errors.password);
-  //     e.preventDefault();
-  //   }
-  //   else if(data.username === "" || !data.username) {
-  //     errors.username = "USERNAME Input is blank. Username length: " + data.username.length + ", " + data.password.length;
-  //     alert(errors.username);
-  //     e.preventDefault();
-  //   }
+  // showSkillForm = () => {
+  //   alert('Skill Button Clicked');
+  // }
   //
-  //   return errors;
+  // showItemForm = () => {
+  //   alert('Item Buttonm Clicked');
+  // }
+  //
+  // showCryptoForm = () => {
+  //   alert('Crypto Button Clicked');
   // }
 
+    // this.setState({
+    //
+    //   //data: { ...this.state.data, [event.target.name]: event.target.value }
+    // });
+    // alert(event.target.name);
+
+
+// DEFAULT IS Add Skill Form
   render() {
-    // const { data } = this.state;
+    const { trade } = this.state;
     return (
       <div className="StartATrade-Component">
         <Card>
           <Container>
             <CardBody>
               <Form onSubmit={this.onSubmit}>
-                <FormGroup>
-                  <CardTitle>start a trade</CardTitle>
-                  <CardSubtitle>put together a trade and find someone to trade with</CardSubtitle>
-                </FormGroup>
-                <FormGroup>
-                  <Button color="primary" onClick={this.handleSignUpSumbit}>add skill</Button>
-                  <Button color="danger" onClick={this.handleSignUpSumbit}>add item</Button>
-                  <Button color="warning" onClick={this.handleSignUpSumbit}>add cryptocurrency</Button>
-                </FormGroup>
+                  <FormGroup>
+                    <CardTitle>start a trade</CardTitle>
+                    <CardSubtitle>put together a trade and find someone to trade with</CardSubtitle>
+                  </FormGroup>
+
+                  <FormGroup>
+                    {/* <button id="1" onClick="reply_click(this.id)">B1</button> */}
+                    <Button name="skillButton" id="skillButton" className="Button-Component"  color="primary" onClick={this.submitButtonClicked}>add skill</Button>
+                    <Button name="itemButton" color="danger" onClick={this.tradeButtonsClicked}>add item</Button>
+                    <Button name="cryptoButton" color="warning" onClick={this.tradeButtonsClicked}>add cryptocurrency</Button>
+                  </FormGroup>
+
                 <FormGroup>
                   <CardTitle>search or select a skill category</CardTitle>
                 </FormGroup>
