7993db21c9d09dfb1e4ca5c2a5c9e03a4bfa7ab3
Alan <boyce.alan15@gmail.com>
2019-01-04 23:04:54 -0500

    set up mongodb

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+  'mongodb://alan:classicrock123@ds149034.mlab.com:49034/2asdatabase'
+};
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('Successfully connected to mongoDB'))
+  .catch(err => console.log("Error!!!: " + err));
+
+app.get('/', (req, res) => res.send('Hello, World'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on ${port}`));
